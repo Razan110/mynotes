2ea8cfaffdad3fc7eec3fbcf0e4697519bc4c208
hoom <hoom@MacBook-Pro-alkhas-b-Mohammed.local>
2023-01-26 08:34:38 +0300

    step-11

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
-
 import '../enums/menu_action.dart';
 
 class NotesView extends StatefulWidget {
