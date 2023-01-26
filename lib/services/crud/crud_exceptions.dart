bebcd40472a8a259db084e69e8fe2bcd7dc98e15
hoom <hoom@MacBook-Pro-alkhas-b-Mohammed.local>
2023-01-26 08:39:50 +0300

    step-11

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,15 @@
+class DatabaseIsAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DtatbaseIsNotOpen implements Exception {}
+
+class CouldNotDelete implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNoteDeletNote implements Exception {}
+
+class CouldNoteFindNote implements Exception {}
